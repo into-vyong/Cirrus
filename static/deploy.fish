4c7b649f8e4bae6991c8aa2a136cda73ed7cc9b9
WincerChan <WincerChan@gmail.com>
2021-03-27 19:48:26 +0800

    update some files

---
+++ static/deploy.fish
@@ -0,0 +1,6 @@
+git init                                                                                                                               
+git add .                                                                                                                              
+git commit -m "Blog Construct"                                                                                                         
+git add layouts/_default/baseof.html
+git remote add origin git@github.com:into-vyong/into-vyong.github.io.git                                                               
+git push origin master -f
