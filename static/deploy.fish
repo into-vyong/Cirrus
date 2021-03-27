610f2fcb1e6a24255f4c1a3a7055607721a7438a
WincerChan <WincerChan@gmail.com>
2021-03-27 19:50:24 +0800

    remove command line

--- static/deploy.fish
+++ static/deploy.fish
@@ -1,6 +1,5 @@
 git init                                                                                                                               
 git add .                                                                                                                              
 git commit -m "Blog Construct"                                                                                                         
-git add layouts/_default/baseof.html
 git remote add origin git@github.com:into-vyong/into-vyong.github.io.git                                                               
 git push origin master -f
